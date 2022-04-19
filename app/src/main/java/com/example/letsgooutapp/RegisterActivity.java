0a1a8e64bd7d1f1932929c8a1b66227375eda8cc
samuel1Y <samuel.bajzik0@gmail.com>
2022-04-19 17:38:42 +0200

    some stuff

--- app/src/main/java/com/example/letsgooutapp/RegisterActivity.java
+++ app/src/main/java/com/example/letsgooutapp/RegisterActivity.java
@@ -2,9 +2,11 @@ package com.example.letsgooutapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -16,7 +18,14 @@ public class RegisterActivity extends AppCompatActivity {
 
 
     public void submitRegistration(View view) {
-        startActivity(new Intent(this, MainActivity.class));
+        finish();
+
+        Context context = getApplicationContext();
+        String text = "Nothing happened, back to login";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast.makeText(context, text, duration).show();
+
 
     }
 }
