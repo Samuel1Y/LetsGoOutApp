e96d0812b7c58cd482ae7bb9069fa28d484d3c0c
terezeRafajova <79629918+terezeRafajova@users.noreply.github.com>
2022-04-20 21:25:54 +0200

    adding Account class

---
+++ app/src/main/java/com/example/letsgooutapp/Account.java
@@ -0,0 +1,32 @@
+package com.example.letsgooutapp;
+
+public class Account {
+    private String username;
+    private String password;
+    private String dateOfBirth;
+    private String email;
+
+    public Account(String username, String email, String dateOfBirth, String password)
+    {
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
--- app/src/main/java/com/example/letsgooutapp/RegisterActivity.java
+++ app/src/main/java/com/example/letsgooutapp/RegisterActivity.java
@@ -6,14 +6,41 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText usernameText;
+    private EditText emailText;
+    private EditText dateText;
+    private EditText passwordText;
+    private EditText password2Text;
+
+    private String username;
+    private String email;
+    private String date;
+    private String password;
+    private String password2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        usernameText = findViewById(R.id.RegisterUsername);
+        emailText = findViewById(R.id.RegisterEmail);
+        dateText = findViewById(R.id.RegisterDate);
+        passwordText = findViewById(R.id.RegisterPassword);
+        password2Text = findViewById(R.id.RegisterPassword2);
+
+        username = usernameText.getText().toString();
+        email = emailText.getText().toString();
+        date = dateText.getText().toString();
+        password = dateText.getText().toString();
+        password2 = dateText.getText().toString();
+
     }
 
 
@@ -21,6 +48,8 @@ public class RegisterActivity extends AppCompatActivity {
         finish();
 
         Context context = getApplicationContext();
+        Account account = new Account(username, email,date,password);
+
         String text = "Nothing happened, back to login";
         int duration = Toast.LENGTH_SHORT;
 
